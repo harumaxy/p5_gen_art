a95a997f9c654171880f74c03948cef9f6f3096d
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2019-12-12 00:52:16 +0900

    オブジェクト作成時に色をランダムに変更する

--- src/models/particle.ts
+++ src/models/particle.ts
@@ -1,4 +1,11 @@
-import p5, { Vector } from 'p5'
+import p5, { Vector, Color } from 'p5'
+
+const Colors = {
+    red: '#f00',
+    green: '#0f0',
+    blue: '#00f',
+    yellow: '#00'
+}
 
 // createVector などは、p5InstanceExtensions インターフェースでしか
 // 実行できない
@@ -7,12 +14,16 @@ export default class Particle {
     vel: Vector
     acc: Vector
     mass: number = 1
+    color: Color
 
-    constructor(x: number, y: number, mass: number, p: p5) {
+    constructor(x: number, y: number, p: p5, mass: number = 1, color?: Color) {
         this.pos = p.createVector(x, y)
         this.vel = p.createVector(0, 0)
         this.acc = p.createVector(0, 0)
         this.mass = mass
+        p.colorMode('hsb', 100)
+        this.color = color || p.color(p.random(0, 100), 100, 100)
+        p.colorMode('rgb', 255)
     }
 
     update = () => {
@@ -23,7 +34,7 @@ export default class Particle {
     }
 
     display = (p: p5) => {
-        p.fill(255)
+        p.fill(this.color)
         p.ellipse(this.pos.x, this.pos.y, this.mass * 10, this.mass * 10)
     }
 
@@ -32,22 +43,26 @@ export default class Particle {
         this.acc.add(acc)
     }
 
-    edge = (p: p5) => {
+    edge = (p: p5, bounce_constant: number = 0.8) => {
         if (this.pos.y > p.height) {
             this.vel.y *= -1
             this.pos.y = p.height
+            this.vel.mult(bounce_constant)
         }
         if (this.pos.y < 0) {
             this.vel.y *= -1
             this.pos.y = 0
+            this.vel.mult(bounce_constant)
         }
         if (this.pos.x > p.width) {
             this.vel.x *= -1
             this.pos.x = p.width
+            this.vel.mult(bounce_constant)
         }
         if (this.pos.x < 0) {
             this.vel.x *= -1
             this.pos.x = 0
+            this.vel.mult(bounce_constant)
         }
     }
 }
